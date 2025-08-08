3a3d737b71b118be04c29affead8c6c7e0fa778c
Johan Mendez <johanoriginalplus@gmail.com>
2025-08-08 08:07:35 -0500

    Comentario para P2

--- P2/utils/src/main/java/co/com/utils/Main.java
+++ P2/utils/src/main/java/co/com/utils/Main.java
@@ -2,6 +2,6 @@ package co.com.utils;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! saludo evaluadores");
+        System.out.println("Hello world! saludo evaluadores, para ver el ejercicio resuelto ir a las pruebas, alla hay varios casos para este ejercicio donde se pone a prueba la funcion que esta en utils");
     }
 }
